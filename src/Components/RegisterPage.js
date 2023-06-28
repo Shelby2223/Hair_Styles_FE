edbf3b4eb4a58a8d6567eac17f180a96ee943919
Trung <dangtrungps@gmail.com>
2023-06-28 14:44:18 +0700

    upall

--- src/Components/RegisterPage.js
+++ src/Components/RegisterPage.js
@@ -1,6 +1,7 @@
 import React, { useState,useEffect } from 'react';
 import axios from 'axios';
 
+
 const RegisterPage = () => {
     const [isPasswordVisible, setPasswordVisible] = useState(false);
     const [isModelVisible, setModelVisible] = useState(false);
