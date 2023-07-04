c518d5a36dee5a4a6d2ee4b234acbd8dd2c9c25a
Trung <dangtrungps@gmail.com>
2023-07-04 12:46:40 +0700

    up phan quyen route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './Components/Header';
 import HomePage from './Components/HomePage';
 import ShopDetailPage from './Components/ShopDetailPage';
@@ -11,75 +11,49 @@ import ContactPage from './Components/ContactPage';
 import HistoryPage from './Components/HistoryPage';
 import Footer from './Components/Footer';
 import Search from './Components/Search';
-// import ForgotPassword from './Components/ForgotPassword';
-import AdminHeader from './Admin/Components/header';
-import Sidenav from './Admin/Components/sidenav';
+import ForgotPassword from './Components/ForgotPassword';
 import BarberShop from './Admin/Components/BarberShop';
 import Income from './Admin/Components/Income';
-
-
-
+import Sidenav from './Admin/Components/sidenav';
+import HeaderAdmin from './Admin/Components/header';
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<LoginPageWithoutHeaderFooter />} />
-        <Route path="/register" element={<RegisterPageWithoutHeaderFooter />} />
-        <Route path="/" element={<DefaultLayout />} />
-        <Route path="/Admin" element={<DefaultAdminLayout/>} />
+  const HAFValue = localStorage.getItem('setHeaderAndFooterHomePage');
+  const hideHeaderFooterHomePage = HAFValue === '0';
 
-      </Routes>
-    </Router>
+  const HAFValue1 = localStorage.getItem('setHeaderAndFooterAdmin');
+  const hideHeaderFooterAdmin = HAFValue1 === '1';
+
+    // localStorage.removeItem('setHeaderAndFooterHomePage');
+    // localStorage.removeItem('setHeaderAndFooterAdmin');
+
+  const abc =localStorage.getItem('setHeaderAndFooterAdmin');
+
+
+  return (
+    <div>
+      <Router>
+        {!hideHeaderFooterHomePage && <Header />}
+        {hideHeaderFooterAdmin && <Sidenav/>}
+        {hideHeaderFooterAdmin && <HeaderAdmin/>}
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/ShopDetailPage/:shop_id" element={<ShopDetailPage />} />
+          <Route path="/Search" element={<Search />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/ShopAdmin" element={<ShopAdminPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/History" element={<HistoryPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/ForgotPassword" element={<ForgotPassword />} />
+          <Route path="/BarberShop" element={<BarberShop />} />
+          <Route path="/Income" element={<Income />} />
+        </Routes>
+        {!hideHeaderFooterHomePage && <Footer />}
+
+      </Router>
+    </div>
   );
-}
-
-function DefaultLayout() {
-  return (
-    <>
-      <Header />
-      <Routes>
-
-        <Route path="/" element={<HomePage />} />
-        <Route path="/ShopDetail" element={<ShopDetailPage />} />
-        <Route path="/Search" element={<Search />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/ShopAdmin" element={<ShopAdminPage />} />
-        <Route path="/contact" element={<ContactPage />} />
-        <Route path="/History" element={<HistoryPage />} />
-      </Routes>
-      <Footer />
-    </>
-  );
-}
-
-function LoginPageWithoutHeaderFooter() {
-  return (
-    <LoginPage />
-  );
-}
-
-function RegisterPageWithoutHeaderFooter() {
-  return (
-    <RegisterPage />
-
-
-);
-}
-function DefaultAdminLayout(){
-  return(
-  <>
-
-  <AdminHeader/>
-  <Sidenav/>
-  
-    <Routes>
-    <Route path="/BarberShop" element={<BarberShop />} />
-     <Route path="/Income" element={<Income />} />
-
-    </Routes>
-
-</>
-)
 }
 
 export default App;
