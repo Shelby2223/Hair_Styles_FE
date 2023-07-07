04fe8acec6e056283b25925786c00c277b07fbe3
hoanb24 <hoan.bui24@student.passerellesnumeriques.org>
2023-07-07 09:47:32 +0700

    Complete Funtion Booking

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,11 +1,275 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "mapbox-gl/dist/mapbox-gl.css";
+import mapboxgl from "mapbox-gl";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./Header";
+import Footer from "./Footer";
+import { Link } from "react-router-dom";
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiaG9hbmIyNCIsImEiOiJjbGpnamV4bXowNmFtM2xxaWZnYnEwaWQ4In0.aSnV4rCTGUxjMMEXOnA9iQ";
 
 const Search = () => {
-    return (
-        <div>
+  const [shops, setShops] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [distanceData, setDistanceData] = useState([]);
+  const [latitude, setLatitude] = useState(null);
+  const [longitude, setLongitude] = useState(null);
+  const [filter, setFilter] = useState("");
+  const [ratingData, setRatingData] = useState([]);
 
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setLatitude(position.coords.latitude);
+          setLongitude(position.coords.longitude);
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
+      console.log([latitude, longitude]);
+    } else {
+      console.error("Geolocation is not supported by this browser.");
+    }
+  }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `http://127.0.0.1:8000/api/shops?search=${searchTerm}`
+        );
+        setShops(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, [searchTerm]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.post(
+          "http://127.0.0.1:8000/api/getdistance",
+          {
+            latitude: latitude,
+            longitude: longitude,
+          }
+        );
+        setDistanceData(response.data.result);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, [latitude, longitude]);
+
+  useEffect(() => {
+    const fetchRatingData = async () => {
+      try {
+        const response = await axios.post(
+          "http://127.0.0.1:8000/api/getratingstar"
+        );
+        setRatingData(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    if (filter === "1") {
+      fetchRatingData();
+    }
+  }, [filter]);
+
+  useEffect(() => {
+    if (latitude && longitude && distanceData.length > 0) {
+      const map = new mapboxgl.Map({
+        container: "map",
+        style: "mapbox://styles/mapbox/streets-v11",
+        center: [longitude, latitude],
+        zoom: 12,
+      });
+
+      // Add marker for your location
+      new mapboxgl.Marker({ color: "blue" })
+        .setLngLat([longitude, latitude])
+        .addTo(map);
+
+      distanceData.forEach((item) => {
+        const {
+          latitude: shopLatitude,
+          longitude: shopLongitude,
+          shop_name,
+          shop_phone,
+        } = item;
+
+        if (shopLatitude && shopLongitude) {
+          new mapboxgl.Marker()
+            .setLngLat([shopLongitude, shopLatitude])
+            .setPopup(
+              new mapboxgl.Popup({ className: 'custom-popup' }).setHTML(
+                `<div style="display: flex; flex-direction: column; align-items: center;">
+                <img src="https://toplisthanoi.com/wp-content/uploads/2021/05/Thanh-cong.jpg" style="width: 70px; height: 70px;" />
+                <div style="font-size:15px;">${shop_name}</div>
+                <ul>
+                  <li ><b> Cách xa bạn ${item.distance} Kilometers </b></li>
+                  <li>Địa chỉ: ${item.address}</li>
+                  <li>Số điện thoại ${shop_phone}</li>
+                </ul>
+              </div>`
+              )
+            )
+
+            .addTo(map);
+        }
+      });
+    }
+  }, [latitude, longitude, distanceData]);
+
+  return (
+    <div>
+
+      <div className="container">
+        <div>
+          <div>
+            <div className="row search-options">
+              <div className="col-md-6">
+                <input
+                  type="text"
+                  value={searchTerm}
+                  onChange={handleSearch}
+                  className="form-control input-search"
+                  placeholder="Search by shop name"
+                />
+              </div>
+              <div className="col-md-6 d-flex justify-content-end">
+                <select
+                  className="form-select align-self-start select-search"
+                  aria-label="Default select example"
+                  value={filter}
+                  onChange={(e) =>
+                    setFilter(e.target.options[e.target.selectedIndex].value)
+                  }
+                >
+                  <option value="">Open this select menu</option>
+                  <option value="1">Rating</option>
+                  <option value="2">Location</option>
+                </select>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-4">
+              {filter === "1" ? (
+                ratingData.map((item) => (
+                  <div key={item.shop_id}>
+                    <div className="shop_name">
+                      <Link className="button" to={`/ShopDetailPage/${item.shop_id}`}>
+                        {" "}
+                        {item.shop_name}
+                      </Link>
+                    </div>
+                    <ul>
+                      <li>Shop ID: {item.shop_id}</li>
+                      <li>Điện thoại: {item.shop_phone}</li>
+                      <li style={{ fontWeight: "bold" }}>
+                        Địa chỉ: {item.address}
+                      </li>
+                      <li style={{ display: "flex" }}>
+                        Đánh giá trung bình: {item.avg_rating}
+                        {[...Array(5)].map((_, index) => (
+                          <span
+                            key={index}
+                            style={{
+                              display: "inline-block",
+                              width: 10,
+                              height: 20,
+                              background: `url("data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 23.44 19'><polygon fill='${
+                                index < Math.floor(item.avg_rating)
+                                  ? "%23fbbc04"
+                                  : "%23999999"
+                              }' points='10,15.27 16.18,19 14.54,11.97 20,7.24 12.81,6.63 10,0 7.19,6.63 0,7.24 5.46,11.97 3.82,19'/></svg>") repeat-x`,
+                            }}
+                          ></span>
+                        ))}
+                      </li>
+                      <li>Số lượt đánh giá: {item.rating_count}</li>
+                    </ul>
+                  </div>
+                ))
+              ) : filter === "2" ? (
+                <div>
+                  <h3>Các cửa hàng cách xa bạn:</h3>
+                  <table className="table table-striped">
+                    <thead>
+                      <tr>
+                        <th>Tên cửa hàng</th>
+                        <th>Địa chỉ</th>
+                        <th>Điện thoại</th>
+                        <th>Khoảng cách (Kilometers)</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {distanceData.map((item) => (
+                        <tr key={item.shop_id}>
+                          <td className="shop_name">
+                            {" "}
+                            <Link
+                              className="button"
+                              to={`/shop/${item.shop_id}`}
+                            >
+                              {" "}
+                              {item.shop_name}
+                            </Link>
+                          </td>
+                          <td style={{ fontWeight: "bold" }}>{item.address}</td>
+                          <td>{item.shop_phone}</td>
+                          <td>{item.distance}</td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+              ) : (
+                <div>
+                  {shops.map((shop) => (
+                    <div key={shop.shop_id}>
+                      <div className="shop_name">
+                        <Link className="button" to={`/shop/${shop.shop_id}`}>
+                          {" "}
+                          {shop.shop_name}
+                        </Link>
+                      </div>
+                      <ul>
+                        <li>Tiệm cắt tóc</li>
+                        <li style={{ fontWeight: "bold" }}>
+                          Địa chỉ: {shop.shop_address}
+                        </li>
+                        <li>Số điện thoại: {shop.shop_phone}</li>
+                      </ul>
+                    </div>
+                  ))}
+                </div>
+              )}
+            </div>
+
+            <div className="col-md-8">
+              <div id="map" style={{ height: "400px", width: "800px" }}></div>
+            </div>
+          </div>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default Search;
--- src/Components/ShowProducts.js
+++ src/Components/ShowProducts.js
@@ -1,57 +1,407 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import Modal from "react-modal";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import "bootstrap/dist/css/bootstrap.css";
+import Swal from "sweetalert2";
+import { format, parseISO } from "date-fns";
 
 const ShowProducts = () => {
-    const [searchResults, setSearchResults] = useState([]);
+  const [data, setData] = useState([]);
+  const [selectedTime, setSelectedTime] = useState(null);
+  const [selectedShop, setSelectedShop] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  // const [selectedService, setSelectedService] = useState(null);
+  const [isDataComplete, setIsDataComplete] = useState(false);
+  const [selectedDateTime, setSelectedDateTime] = useState("");
+  const [selectedStylist, setSelectedStylist] = useState(null);
+  // const [isStylistBooked, setIsStylistBooked] = useState(false);
+  const [stylistAvailability, setStylistAvailability] = useState({});
+  const [searchResults, setSearchResults] = useState([]);
 
-    const fetchData = async () => {
-        try {
-            const response = await axios.get('http://127.0.0.1:8000/api/shops');
-            setSearchResults(response.data);
-        } catch (error) {
-            console.log(error);
-        }
-    };
+  const fetchDataold = async () => {
+    try {
+      const response = await axios.get("http://127.0.0.1:8000/api/shops");
+      setSearchResults(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    useEffect(() => {
-        fetchData();
-    }, []);
+  useEffect(() => {
+    fetchDataold();
+    fetchData();
+  }, []);
 
-    return (
-        <div>
-            <div className='showproduct'>
-                <br />
-                <h2>Sản phẩm</h2>
-              
-                <br /><br />
-                
-                <div className="row" style={{marginLeft:"30px",marginRight:'30px'}}>
-                    {searchResults.map((e) => (
-                        <div className="col-md-4" key={e.shop_id}>
-                            <div className="card">
-                                <img
-                                    src={`../assets/img/shops/${e.shop_image}`}
-                                    alt=""
-                                    className="card-img-top"
-                                />
-                                <div className="card-body">
-                                    <h4 className="card-title">{e.shop_name}</h4>
-                                    <p className="card-text"> Phone: {e.shop_phone}</p>
-                                    <p className="card_price">{e.user}</p>
-                                    <div className="function">
-                                        <Link className="button" to={`/ShopDetailPage/${e.shop_id}`}>
-                                            Chi tiết Shop
-                                        </Link>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    ))}
-                </div>
-            </div>
-        </div>
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("http://127.0.0.1:8000/api/getservices");
+      setData(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  function handleBooking(service, shop) {
+    setSelectedShop({ service, shop });
+    setIsModalOpen(true);
+  }
+
+  function handleModalClose() {
+    setIsModalOpen(false);
+    setSelectedStylist(null);
+  }
+
+  function handleDateTimeSelection(event) {
+    const selectedDateTimeValue = event.target.value;
+    setSelectedDateTime(selectedDateTimeValue);
+    const selectedDateTime = new Date(event.target.value);
+    const currentDate = new Date();
+    const selectedDateTimeObj = new Date(selectedDateTime);
+
+    if (selectedDateTimeObj < currentDate) {
+      Swal.fire("Lỗi", "Vui lòng chọn một thời gian trong tương lai.", "error");
+      return;
+    }
+
+    const differenceInDays = Math.floor(
+      (selectedDateTime - currentDate) / (1000 * 60 * 60 * 24)
     );
+
+    if (differenceInDays > 3) {
+      Swal.fire({
+        icon: "error",
+        title: "Lỗi",
+        text: "Ngày đặt lịch phải nhỏ hơn hoặc bằng 3 ngày từ ngày hiện tại.",
+      });
+    } else {
+      setSelectedTime(event.target.value);
+    }
+  }
+
+  useEffect(() => {
+    setIsDataComplete(selectedTime !== null && selectedStylist !== null);
+  }, [selectedTime, selectedStylist]);
+
+  const handleConfirm = async () => {
+    if (!selectedTime) {
+      Swal.fire("Lỗi", "Vui lòng chọn thời gian.", "error");
+      return;
+    }
+
+    if (!selectedStylist) {
+      Swal.fire("Lỗi", "Vui lòng chọn stylist.", "error");
+      return;
+    }
+
+    const isStylistAvailable = await checkStylistAvailability(
+      selectedStylist,
+      selectedDateTime
+    );
+
+    if (!isStylistAvailable) {
+      Swal.fire("Lỗi", "Stylist đã được đặt lịch vào thời gian này.", "error");
+      return;
+    }
+
+    // Tiếp tục xử lý khi stylist khả dụng
+    Swal.fire("Thành công", "Đặt lịch thành công", "success");
+    handleModalClose();
+
+    if (selectedShop && selectedShop.service) {
+      const { shop, service } = selectedShop;
+      const { service_name, service_price } = service;
+
+      // Format the selectedDateTime
+      const formattedDateTime = format(
+        parseISO(selectedDateTime),
+        "yyyy-MM-dd HH:mm:ss"
+      );
+      const formattedTime = format(parseISO(selectedDateTime), "HH:mm");
+      const formattedDate = format(parseISO(selectedDateTime), "dd/MM");
+
+      const productData = {
+        shop_id: shop.shop_id,
+        shop_name: shop.shop_name,
+        service_id: service.service_id,
+        service_name: service_name,
+        price: service_price,
+        appointment_time: formattedTime,
+        appointment_date: formattedDate,
+        stylist_id: selectedStylist,
+      };
+
+      try {
+        // Get the existing product data from sessionStorage
+        const existingProductData =
+          JSON.parse(sessionStorage.getItem("productData")) || [];
+
+        // Make sure existingProductData is an array
+        if (!Array.isArray(existingProductData)) {
+          throw new Error("Existing product data is not an array.");
+        }
+
+        // Add the new product to the existing data
+        const updatedProductData = [...existingProductData, productData];
+
+        // Store the updated product data in sessionStorage
+        sessionStorage.setItem(
+          "productData",
+          JSON.stringify(updatedProductData)
+        );
+      } catch (error) {
+        console.error("Error updating product data:", error);
+      }
+    }
+  };
+
+  const customModalStyles = {
+    content: {
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+      width: "50%",
+      maxHeight: "80vh",
+      overflow: "auto",
+    },
+  };
+
+  const handleStylistSelection = (event) => {
+    const selectedStylistId = event.target.value;
+    setSelectedStylist(selectedStylistId);
+
+    checkStylistAvailability(selectedStylistId, selectedDateTime)
+      .then((isStylistAvailable) => {
+        setStylistAvailability((prevAvailability) => ({
+          ...prevAvailability,
+          [selectedStylistId]: isStylistAvailable,
+        }));
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  const checkStylistAvailability = async (
+    stylistId,
+    appointmentDate,
+    appointmentTime
+  ) => {
+    try {
+      // Định dạng lại giờ và ngày
+      const selectedDateTime = new Date(appointmentDate);
+      const formattedTime = `${selectedDateTime.getHours()}:${selectedDateTime.getMinutes()}`;
+      const formattedDate = `${selectedDateTime.getDate()}/${
+        selectedDateTime.getMonth() + 1
+      }`;
+
+      console.log("stylistId:", stylistId);
+      console.log("appointmentDate:", formattedDate);
+      console.log("appointmentTime:", formattedTime);
+
+      const response = await axios.post(
+        "http://127.0.0.1:8000/api/getbookedstylists",
+        {
+          stylist_id: stylistId,
+          appointment_date: formattedDate,
+          appointment_time: formattedTime,
+        }
+      );
+
+      // console.log(response.data); // Log dữ liệu từ response
+
+      const isStylistAvailable = response.data.available;
+
+      return isStylistAvailable;
+    } catch (error) {
+      console.error(error);
+      return false;
+    }
+  };
+
+  return (
+    <>
+      <section className="showproduct container">
+        <br />
+        <h2>Các shop</h2>
+        <br />
+        <br />
+        <div
+          className="row"
+          style={{ marginLeft: "30px", marginRight: "30px" }}
+        >
+          {searchResults.map((e) => (
+            <div className="col-md-4" key={e.shop_id}>
+              <div className="card">
+                <img
+                  src={`../assets/img/shops/${e.shop_image}`}
+                  alt=""
+                  className="card-img-top img-shop"
+                />
+                <div className="card-body">
+                  <h4 className="card-title">{e.shop_name}</h4>
+                  <p className="card-text"> Phone: {e.shop_phone}</p>
+                  <p className="card_price">{e.user}</p>
+                  <div className="function">
+                    <Link
+                      className="button"
+                      to={`/ShopDetailPage/${e.shop_id}`}
+                    >
+                      Chi tiết Shop
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+
+      <div className="showproduct container">
+        <br />
+        <h3 className="colection">DANH SÁCH SẢN PHẨM PHỤC VỤ</h3>
+        <br />
+        <br />
+
+        {Object.keys(data).length > 0 ? (
+          Object.keys(data).map((shopId) => {
+            const shop = data[shopId];
+
+            const sliderSettings = {
+              dots: false,
+              infinite: true,
+              speed: 500,
+              slidesToShow: 3,
+              slidesToScroll: 1,
+              centerMode: true,
+              centerPadding: "20px",
+              prevArrow: <button className="slick-prev">Previous</button>,
+              nextArrow: <button className="slick-next">Next</button>,
+              responsive: [
+                {
+                  breakpoint: 768,
+                  settings: {
+                    slidesToShow: 1,
+                    centerPadding: "20px",
+                  },
+                },
+              ],
+            };
+
+            return (
+              <div className="shop-row row" key={shop.shop_id}>
+                <h4 className="shop-name">{shop.shop_name}</h4>
+                <Slider {...sliderSettings}>
+                  {shop.services.map((service) => (
+                    <div className="d-flex" key={service.service_id}>
+                      <div className="card">
+                        <img
+                          src={`../assets/img/service/${service.service_image}`}
+                          alt={service.service_name}
+                          className="card-img-top object-fit-cover"
+                        />
+                        <div className="card-body">
+                          <h4 className="card-title text-dark">
+                            {service.service_name}
+                          </h4>
+                          <p className="card-text">{service.service_price}</p>
+                          <div className="function d-flex justify-content-around">
+                            <Link
+                              className="btn btn-primary p-3"
+                              to={`/shop/${shop.shop_id}`}
+                            >
+                              Chi tiết Shop
+                            </Link>
+                            <button
+                              className="btn btn-primary p-3"
+                              onClick={() => handleBooking(service, shop)}
+                            >
+                              Đặt ngay
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  ))}
+                </Slider>
+              </div>
+            );
+          })
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
+
+      <Modal
+        isOpen={isModalOpen}
+        contentLabel="Chọn lịch trống"
+        style={customModalStyles}
+      >
+        <div className="d-flex justify-content-between">
+          <h2>Chọn lịch trống</h2>
+          <button
+            className="btn btn-secondary btn-sm float-start"
+            onClick={handleModalClose}
+          >
+            X
+          </button>
+        </div>
+        {selectedShop && (
+          <>
+            <div>
+              <p>Shop được chọn: {selectedShop.shop.shop_name}</p>
+            </div>
+            <h4>Dịch vụ:</h4>
+            <div>
+              {selectedShop && (
+                <>
+                  <p>Tên dịch vụ: {selectedShop.service.service_name}</p>
+                  <p>Giá: {selectedShop.service.service_price}</p>
+                  <p>Thời gian: {selectedTime}</p>
+                </>
+              )}
+            </div>
+            {selectedShop && selectedTime && (
+              <div>
+                <h3>Stylist:</h3>
+                <select onChange={handleStylistSelection}>
+                  <option value="">Chọn stylist</option>
+                  {selectedShop.shop.stylists.map((stylist) => (
+                    <option
+                      key={stylist.stylist_id}
+                      value={stylist.stylist_id}
+                      // disabled={!stylistAvailability[stylist.stylist_id]}
+                    >
+                      {stylist.stylist_name}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            )}
+          </>
+        )}
+        <label htmlFor="dateInput">Ngày và giờ đặt lịch:</label>
+        <input
+          type="datetime-local"
+          id="dateInput"
+          onChange={handleDateTimeSelection}
+          required
+          className="mt-3"
+        />
+        <br></br>
+        <button onClick={handleConfirm} className="btn btn-primary">
+          Xác nhận
+        </button>
+      </Modal>
+    </>
+  );
 };
 
 export default ShowProducts;
